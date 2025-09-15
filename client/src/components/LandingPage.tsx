cac6014637ffe125764ad71d13b0b90faf44832c
ChidexWorld <chiderastanley272@gmail.com>
2025-09-15 03:51:51 +0100

    final touches , updated the meta data

--- client/src/components/LandingPage.tsx
+++ client/src/components/LandingPage.tsx
@@ -318,7 +318,7 @@ const LandingPage: React.FC = () => {
           </div>
 
           <div className="border-t border-gray-800 mt-8 pt-8 text-center text-sm text-gray-400">
-            <p>&copy; 2024 MediCheck. All rights reserved. For educational purposes only.</p>
+            <p>&copy; 2025 MediCheck. All rights reserved. For educational purposes only.</p>
           </div>
         </div>
       </footer>
